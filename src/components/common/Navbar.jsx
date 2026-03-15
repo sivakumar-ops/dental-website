402f883c9859915c36ebbe8724f4fd8a96a28a05
sivakumar-ops <skjavatechie@gmail.com>
2026-03-15 17:05:14 +0530

    Update Navbar.jsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -54,14 +54,14 @@ const Navbar = () => {
 
             {/* CTA Button */}
             <div className={styles.navActions}>
-              <a href="tel:5005555050" className={styles.phoneLink} aria-label="Call us at (500) 555-5050">
+              <a href="tel:9133333552" className={styles.phoneLink} aria-label="Call us at 9133333552">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" aria-hidden="true">
                   <path d="M2 3C2 2.44772 2.44772 2 3 2H5.15287C5.64171 2 6.0589 2.35341 6.13927 2.8356L6.87858 7.27147C6.95075 7.70451 6.73206 8.13397 6.3394 8.3303L4.79126 9.10437C5.90756 11.8783 8.12168 14.0924 10.8956 15.2087L11.6697 13.6606C11.866 13.2679 12.2955 13.0492 12.7285 13.1214L17.1644 13.8607C17.6466 13.9411 18 14.3583 18 14.8471V17C18 17.5523 17.5523 18 17 18H15C7.8203 18 2 12.1797 2 5V3Z" fill="currentColor"/>
                 </svg>
                 <span>9133333552</span>
               </a>
               <Link to="/contact-us" className={styles.ctaButton}>
-                Let's Get in Touch
+                Contact us
               </Link>
             </div>
 
@@ -108,8 +108,8 @@ const Navbar = () => {
                   animate={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.3, delay: 0.1 }}
                 >
-                  <a href="tel:5005555050" className={styles.mobilePhone}>
-                    📞 (500) 555-5050
+                  <a href="tel:9133333552" className={styles.mobilePhone}>
+                    📞 9133333552
                   </a>
                 </motion.li>
               </ul>
